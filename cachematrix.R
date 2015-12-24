5f90e934b9745a6a85c8b38246fa0a9fc85adb4b
idiasuley <eniolasuley@gmail.com>
2015-12-23 19:35:14 -0600

    2nd commit: improved readability

--- cachematrix.R
+++ cachematrix.R
@@ -9,15 +9,21 @@
 
 makeCacheMatrix <- function(x = matrix()) {
   inv <- NULL
-  setmatrix <- function(y){
-    x <<- y
+  setmatrix <- function(y){ 
+    x <<- y 
+    ##sets the matrix entered by user as the matrix to be inverted
     inv <<- NULL
+    ## initializes inverse matrix for matrix y as null 
   }
   getmatrix <- function() x
+  ## gets the matrix entered 
   setcache <- function(solve) inv <<- solve
+  ## set the inverse as the cached value
   getinverse <- function() inv
+  ## gets the cached value
   list(setmatrix=setmatrix, getmatrix=getmatrix, 
        setcache=setcache, getinverse=getinverse)
+  ## creates a list of all functions returned by makecachematrix
 }
 
 ## This function checks if the inverse for the specific matrix has been 
@@ -26,13 +32,17 @@ makeCacheMatrix <- function(x = matrix()) {
 ## function
 
 cacheSolve <- function(x, ...) {
-  inv <- x$getinverse()
+  inv <- x$getinverse() ## sets inverse as last cached value
   if (!is.null(inv)){
     message ("Getting cached data...")
     return(inv)
+    ## checks and returns inverse only if it is not a null value
   }
   tobesolved <- x$getmatrix()
+  ## gets matrix to be inverted from makecachematrix list
   inv <- solve(tobesolved, ...)
-  x$setcache(inv)
-  inv        ## Return a matrix that is the inverse of 'x'
+  ## calculates the inverse of the matrix using the solve function
+  
+  x$setcache(inv) ## sets new inverse as the cached inverse
+  inv        ## returns a matrix that is the inverse of 'x'
 }
